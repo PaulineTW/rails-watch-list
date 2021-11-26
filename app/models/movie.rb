06cde055f8a323dd020a799734141fa337f00d58
pauline rouger <paulinerouge@hotmail.com>
2021-11-26 17:53:15 +0800

    Fetch movies from API + added style to views

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
   validates :title, :overview, presence: true
   validates :title, uniqueness: true
 end
