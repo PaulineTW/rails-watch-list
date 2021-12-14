f6df223de564de0d9d897d91e79acd7baf2be0c9
pauline rouger <paulinerouge@hotmail.com>
2021-12-14 12:47:56 +0800

    navbar improvement + add my lists button

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ Movie.destroy_all
 puts "Database cleaned"
 
 url = "http://tmdb.lewagon.com/movie/top_rated"
-10.times do |i|
+100.times do |i|
   puts "Importing movies from page #{i + 1}"
   movies = JSON.parse(open("#{url}?page=#{i + 1}").read)['results']
   movies.each do |movie|
@@ -24,4 +24,3 @@ url = "http://tmdb.lewagon.com/movie/top_rated"
   end
 end
 puts "Movies created"
-
