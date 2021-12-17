3da8fe1c9069f3c7f6ff57cbd5f091a0d412a906
pauline rouger <paulinerouge@hotmail.com>
2021-12-17 13:37:22 +0800

    movie card display

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,5 +4,10 @@ class MoviesController < ApplicationController
     if params[:query].present?
       @movies = @movies.where('title ILIKE ?', "%#{params[:query]}%")
     end
+
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: 'movies/list', locals: { movies: @movies }, formats: [:html] }
+    end
   end
 end
