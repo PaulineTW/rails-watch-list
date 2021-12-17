8ba767216ff299eb1d80f59fa480a17f106a63f5
pauline rouger <paulinerouge@hotmail.com>
2021-12-17 16:28:50 +0800

    ajaxed search

--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -3,12 +3,15 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ['form', 'input', 'list'];
 
-  // connect() {
-  //   console.log(this.formTarget);
-  //   console.log(this.inputTarget);
-  //   console.log(this.listTarget);
-  // }
+  connect() {
+    console.log(this.formTarget);
+    console.log(this.inputTarget);
+    console.log(this.listTarget);
+  }
   
+  update(event) {
+    console.log('TODO: send request in AJAX');
+  }
   update(event) {
     const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
     fetch(url, { headers: { 'Accept': 'text/plain' } })
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_one_attached :photo
+  has_many :lists, through: :bookmarks
   validates :title, :overview, presence: true
   validates :title, uniqueness: true
 end
